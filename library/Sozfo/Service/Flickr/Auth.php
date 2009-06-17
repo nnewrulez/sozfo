a25cab4a16e124e24c5c7f57bd08b82053232dd2
jurian@soflomo.com <jurian@soflomo.com@2b7ecc38-5b16-11de-a2a3-1fafad8a8996>
2009-06-17 09:01:47 +0000

    Added two services from the Soflomo library: Flickr and Tumblr.

---
+++ library/Sozfo/Service/Flickr/Auth.php
@@ -0,0 +1,39 @@
+<?php
+class Sozfo_Service_Flickr_Auth extends Sozfo_Service_Flickr_Abstract
+{
+    const format = 'http://flickr.com/services/auth/?api_key=%s&perms=%s&extra=%s&api_sig=%s';
+
+    protected $_perms = 'read';
+
+    public function setPerms ($perms)
+    {
+        $this->_perms = (string) $perms;
+        return $this;
+    }
+
+    public function getPerms ()
+    {
+        return $this->_perms;
+    }
+
+    public function getFrobUrl(){
+        $redirect = 'http://' . $_SERVER[ 'SERVER_NAME' ] . $_SERVER[ 'REQUEST_URI' ];
+        $url = sprintf(self::format,
+                       $this->getBroker()->getKey(),
+                       $this->getPerms(),
+                       urlencode($redirect),
+                       md5($this->getBroker()->getSecret()
+                        . 'api_key'
+                        . $this->getBroker()->getKey()
+                        . 'extra'
+                        . $redirect
+                        . 'permsread'));
+        return $url;
+    }
+
+    public function getTokenFromFrob ($frob)
+    {
+        $response = $this->_request('auth.getToken', array('frob'=>$frob));
+        return $response['auth']['token'];
+    }
+}
