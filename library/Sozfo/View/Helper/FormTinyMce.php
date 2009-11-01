9d0af56bfd16b7f74b26044fbb9b698c26ee7a88
jurian@soflomo.com <jurian@soflomo.com@2b7ecc38-5b16-11de-a2a3-1fafad8a8996>
2009-11-01 21:34:28 +0000

    Added phpdoc comment for the files. File comment (for license) and class comment (for explaination how it works).

--- library/Sozfo/View/Helper/FormTinyMce.php
+++ library/Sozfo/View/Helper/FormTinyMce.php
@@ -1,4 +1,31 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category  Sozfo
+ * @package   Sozfo_Form
+ * @copyright Copyright (c) 2009 Soflomo V.O.F. (http://www.soflomo.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * Sozfo View Helper FormTinyMce
+ *
+ * The rendering of the view element. Using the TinyMce view helper javascript
+ * initiazion.
+ * @author jurian
+ *
+ */
 class Sozfo_View_Helper_FormTinyMce extends Zend_View_Helper_FormTextarea
 {
     protected $_tinyMce;
