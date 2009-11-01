6cc7ec95d084b00374218f4b1bb514c1360286e4
jurian@soflomo.com <jurian@soflomo.com@2b7ecc38-5b16-11de-a2a3-1fafad8a8996>
2009-11-01 21:34:28 +0000

    Added phpdoc comment for the files. File comment (for license) and class comment (for explaination how it works).

--- trunk/library/Sozfo/Log/Writer/Twitter.php
+++ trunk/library/Sozfo/Log/Writer/Twitter.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category  Sozfo
+ * @package   Sozfo_Form
+ * @copyright Copyright (c) 2009 Soflomo V.O.F. (http://www.soflomo.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * Sozfo Log Writer Twitter
+ *
+ * Used to write log messages as updates for a Twitter account.
+ * @author jurian
+ *
+ */
 class Sozfo_Log_Writer_Twitter extends Zend_Log_Writer_Abstract
 {
     protected $_username;
