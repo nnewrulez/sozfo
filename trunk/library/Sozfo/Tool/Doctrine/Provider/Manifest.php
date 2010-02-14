45343436dc9c360e58801b93473f116fea91db4b
jurian@soflomo.com <jurian@soflomo.com@2b7ecc38-5b16-11de-a2a3-1fafad8a8996>
2010-02-14 21:48:00 +0000

    Doctrine now finally works -sort of-

--- trunk/library/Sozfo/Tool/Doctrine/Provider/Manifest.php
+++ trunk/library/Sozfo/Tool/Doctrine/Provider/Manifest.php
@@ -4,8 +4,13 @@ class Sozfo_Tool_Doctrine_Provider_Manifest implements Zend_Tool_Framework_Manif
     public function getProviders ()
     {
         require_once 'Sozfo/Tool/Doctrine/Provider/Doctrine.php';
+        require_once 'Sozfo/Tool/Doctrine/Provider/DoctrineProject.php';
+        require_once 'Sozfo/Tool/Doctrine/Provider/DoctrineMigrations.php';
+        
         return array(
             new Sozfo_Tool_Doctrine_Provider_Doctrine,
+            new Sozfo_Tool_Doctrine_Provider_DoctrineProject,
+            new Sozfo_Tool_Doctrine_Provider_DoctrineMigrations,
         );
     }
 }
